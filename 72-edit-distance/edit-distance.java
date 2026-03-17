9ef8643373988fd4e503c03f0110d06b8f151839
BHARGAVSAI558 <bhargavsai954@gmail.com>
2026-03-17 22:46:05 +0530

    Time: 4 ms (77.80%) | Memory: 46.8 MB (89.91%) - LeetSync

--- 72-edit-distance/edit-distance.java
+++ 72-edit-distance/edit-distance.java
@@ -9,12 +9,14 @@ class Solution {
         return z;
 
     }
+    
     public int minDistance(String word1, String word2) {
         int m=word1.length();
         int n=word2.length();
 
         int dp[][]=new int[m+1][n+1];
-
+          char []s1=word1.toCharArray();
+            char []s2=word2.toCharArray();
         for(int i=0;i<n+1;i++){
             dp[0][i]=i;
         }
@@ -25,8 +27,7 @@ class Solution {
         for(int i=1;i<m+1;i++){
          for(int j=1;j<n+1;j++)
          {
-            char []s1=word1.toCharArray();
-            char []s2=word2.toCharArray();
+          
 
             if(s1[i-1]==s2[j-1])
             dp[i][j]=dp[i-1][j-1];
