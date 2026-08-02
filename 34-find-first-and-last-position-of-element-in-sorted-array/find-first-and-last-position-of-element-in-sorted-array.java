218f1a673aab8ce17891ed9f622ea023fe23bfbb
BHARGAVSAI558 <bhargavsai954@gmail.com>
2026-08-02 12:24:40 +0530

    Time: 0 ms (100.00%) | Memory: 48.1 MB (52.35%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.java
@@ -1,52 +1,50 @@
 class Solution {
 
-    public int first(int nums[],int n)
-    {
-        int low=0;
-        int high=nums.length-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==n){
-                ans=mid;
-                  high=mid-1;
-            }
-            else if(nums[mid]>n){
-                 high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
+    public int first(int nums[],int k){
+    int ans=-1;
+    int low=0;
+    int high=nums.length-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==k){
+            ans=mid;
+            high=mid-1;
         }
-
-    return ans;
-
+        else if(nums[mid]>k){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
     }
-    public int last(int nums[],int n){
-          int low=0;
-        int high=nums.length-1;
-        int ans=-1;
-        while(low<=high){
-            int mid=low+(high-low)/2;
-            if(nums[mid]==n){
-               low=mid+1;
-                ans=mid;
-            }
-            else if(nums[mid]>n){
-                 high=mid-1;
-            }
-            else{
-                low=mid+1;
-            }
-        }
 
     return ans;
-
     
     }
+    public int last(int nums[],int k){
+ int ans=-1;
+    int low=0;
+    int high=nums.length-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(nums[mid]==k){
+            ans=mid;
+            low=mid+1;
+        }
+        else if(nums[mid]>k){
+            high=mid-1;
+        }
+        else{
+            low=mid+1;
+        }
+    }
 
+    return ans;
+    }
+
+       
     public int[] searchRange(int[] nums, int target) {
         
-        return new int[]{ first(nums,target),last(nums,target)};
+        return new int[]{first(nums,target),last(nums,target)};
     }
 }
